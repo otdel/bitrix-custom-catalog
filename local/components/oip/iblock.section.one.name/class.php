d39ee408f3ccb17dc170894bc58ee089fb06d5ee
Ilya Fertikov <rodeljob@gmail.com>
2019-11-06 12:01:53 +0400

    mlk-110 - имя категории - компонент по получению имени категории

---
+++ local/components/oip/iblock.section.one.name/class.php
@@ -0,0 +1,34 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+require_once(__DIR__."/../Section.php");
+
+\CBitrixComponent::includeComponentClass("oip:iblock.section.list");
+
+class COipIblockSectionOneName extends \COipIblockSectionList
+{
+
+    public function executeComponent()
+    {
+        if(empty($this->arResult["EXCEPTION"])) {
+            try {
+                if (!\Bitrix\Main\Loader::includeModule("iblock")) {
+                    throw new \Bitrix\Main\SystemException("Module iblock is not installed");
+                }
+
+                $this->execute();
+
+                return reset($this->arResult["SECTIONS"])->getName();
+
+            } catch (LoaderException $e) {
+                return  $e->getMessage();
+            }
+            catch (SystemException $e) {
+                return $e->getMessage();
+            }
+        }
+        else {
+            return $this->arResult["EXCEPTION"];
+        }
+    }
+}
