04bb5ff55bca41b203acca558edcdb2be2d5d3a7
Ilya Fertikov <rodeljob@gmail.com>
2019-11-18 12:58:09 +0400

    codefix: попытка получить id от объекта несуществующего свойства

--- local/components/oip/iblock.element.list/templates/.default/include/brands.php
+++ local/components/oip/iblock.element.list/templates/.default/include/brands.php
@@ -2,13 +2,16 @@
 /** @var $component \COipIblockElementList */
 /** @var \Oip\Custom\Component\Iblock\Element $element */
 ?>
-<?$APPLICATION->IncludeComponent(
-    "oip:iblock.element.list","brands-in-list",
-    [
-        "IBLOCK_ID" => $element->getProp("BRANDS")->getLinkIblockId(),
-        "FILTER" => ["ID" => $element->getPropValue("BRANDS")],
-        "SHOW_ALL" => "Y",
-        "IS_CACHE" => $component->getParam("IS_CACHE"),
-        "CACHE_TIME" => $component->getParam("CACHE_TIME"),
-    ]
-)?>
+<?if($element->getProp("BRANDS")):?>
+    <?$APPLICATION->IncludeComponent(
+        "oip:iblock.element.list","brands-in-list",
+        [
+            "IBLOCK_ID" => $element->getProp("BRANDS")->getLinkIblockId(),
+            "FILTER" => ["ID" => $element->getPropValue("BRANDS")],
+            "SHOW_ALL" => "Y",
+            "IS_CACHE" => $component->getParam("IS_CACHE"),
+            "CACHE_TIME" => $component->getParam("CACHE_TIME"),
+        ]
+    )?>
+<?endif?>
+
