4854e3de71a461f081490208937236406f9c82a6
Ilya Fertikov <rodeljob@gmail.com>
2019-11-28 11:40:54 +0400

    mlk-18 - метод по выводу окончаний русский слов по числу

--- local/components/oip/component/class.php
+++ local/components/oip/component/class.php
@@ -175,4 +175,30 @@ abstract class COipComponent extends \CBitrixComponent
 
         return $result;
     }
+
+    /**
+     * @param string $value
+     * @param array $words
+     * @param boolean $show
+     * 
+     * @return string
+     */
+    public function getNumWord($value, $words, $show = false)
+    {
+        $num = $value % 100;
+        if ($num > 19) {
+        $num = $num % 10;
+        }
+
+        $out = ($show) ?  $value . ' ' : '';
+        switch ($num) {
+            case 1:  $out .= $words[0]; break;
+            case 2:
+            case 3:
+            case 4:  $out .= $words[1]; break;
+            default: $out .= $words[2]; break;
+        }
+
+        return $out;
+    }
 }
