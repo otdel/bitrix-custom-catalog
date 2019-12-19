cd1e6f17e59545be408d9d859e51cea6992ed6b4
Ilya Fertikov <rodeljob@gmail.com>
2019-12-19 15:31:21 +0400

    mlk-156 - корзина: компонент-страница

---
+++ local/components/oip/social.store.cart.page/templates/.default/template.php
@@ -0,0 +1,87 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/** @var array $arResult */
+/** @var $this \CBitrixComponentTemplate */
+/** @var $component \COipSocialStoreCartPage */
+/** @var $products \Oip\SocialStore\Product\Entity\ProductCollection */
+/** @var $cart \Oip\SocialStore\Cart\Handler */
+
+$cart =  $arResult["CART"];
+$products = $arResult["CART"]->getProducts();
+$component = $this->getComponent();
+?>
+
+<div class="uk-container">
+    <div class="uk-padding">
+
+        <?if(!$products->count()):?>
+            <table class="uk-table">
+                <caption>Ваша корзина пуста</caption>
+            </table>
+        <?else:?>
+
+            <table class="uk-table">
+                <caption>В вашей корзине  <?=$products->count()?> <?=$component->getNumWord($products->count(),
+                        ["товар","товара","товаров"])?></caption>
+
+                <thead>
+                <tr>
+                    <th>Товар</th>
+                    <th>Изображение</th>
+                    <th>Описание</th>
+                    <th>Дествия</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <?foreach($products->getArray() as $product):?>
+                    <tr>
+
+                        <td><?=$product->getName()?></td>
+                        <td>
+                            <?if($product->getLink()):?>
+                            <a href="<?=$product->getLink()?>" title="<?=$product->getName()?>">
+                                <?endif?>
+
+                                <img src="<?=$product->getPicture()?>" alt="<?=$product->getName()?>" width="150">
+                                <?if($product->getLink()):?>
+                            </a>
+                        <?endif?>
+                        </td>
+                        <td><?=$product->getDescription()?></td>
+                        <td>
+                            <form action="" class="uk-form" method="post">
+                                <input type="hidden" name="<?=$cart::GLOBAL_CART_DATA_PRODUCT_ID?>" value="<?=$product->getId()?>">
+                                <button class="uk-button" name="<?=$cart::GLOBAL_CART_ACTION_NAME?>"
+                                        value="<?=$cart::GLOBAL_CART_ACTION_REMOVE_PRODUCT?>">
+                                    <i class="uk-icon" uk-icon="close" ></i>
+                                    Удалить
+                                </button>
+                            </form>
+
+                        </td>
+                    </tr>
+                <?endforeach?>
+                </tbody>
+
+                <tfoot>
+                <tr>
+                    <td colspan="3">
+                        <form action="" class="uk-form" method="post">
+                            <button class="uk-button" name="<?=$cart::GLOBAL_CART_ACTION_NAME?>"
+                                    value="<?=$cart::GLOBAL_CART_ACTION_REMOVE_ALL?>">
+                                    <i class="uk-icon" uk-icon="trash" ></i>
+                                Очистить корзину
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                </tfoot>
+
+            </table>
+
+        <?endif?>
+
+    </div>
+</div>
