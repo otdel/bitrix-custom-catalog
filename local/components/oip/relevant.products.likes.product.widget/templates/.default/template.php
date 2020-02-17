0859260812835af7fd6007a20a18e38030f99e7e
Ilya Fertikov <rodeljob@gmail.com>
2020-02-17 15:54:39 +0400

    mlk-178 - в компонент виджета лайка добавлена форма отправки запроса на добавление/удаление лайка

--- local/components/oip/relevant.products.likes.product.widget/templates/.default/template.php
+++ local/components/oip/relevant.products.likes.product.widget/templates/.default/template.php
@@ -1,14 +1,22 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
+use Oip\RelevantProducts\DataWrapper;
+
 /** @var array $arResult */
 /** @var Exception $exception */
+/** @var CBitrixComponentTemplate $this  */
+/** @var CRelevantProductsLikesProductWidget $component  */
 
+$component = $this->getComponent();
 $exception = $arResult["EXCEPTION"];
 $likes = (is_null($exception)) ? $arResult["LIKES"] : null;
 $isLiked = (is_null($exception)) ? $arResult["IS_LIKED"] : null;
 $isLikedCss = ($isLiked) ? "uk-button-primary" : "";
 $isLikedTooltip = ($isLiked) ? "Убрать из отложенных" : "Отложить";
+
+$actionValue = ($isLiked) ? DataWrapper::GLOBAL_PRODUCT_LIKE_ACTION_REMOVE
+    : DataWrapper::GLOBAL_PRODUCT_LIKE_ACTION_ADD;
 ?>
 
 <?if(!is_null($exception)):?>
@@ -16,6 +24,11 @@ $isLikedTooltip = ($isLiked) ? "Убрать из отложенных" : "От�
         "EXCEPTION" => $exception
     ])?>
 <?else:?>
-    <?=$likes?>&nbsp;<button class="uk-icon-button uk-margin-small-right <?=$isLikedCss?>"
-                             uk-icon="heart" uk-tooltip="<?=$isLikedTooltip?>"></button>
+    <form action="" method="post">
+        <input type="hidden" name="<?=DataWrapper::GLOBAL_PRODUCT_LIKE_ACTION_NAME?>" value="<?=$actionValue?>">
+        <input type="hidden" name="<?=DataWrapper::GLOBAL_PRODUCT_LIKE_PRODUCT_ID?>"
+               value="<?=$component->getParam("PRODUCT_ID")?>">
+        <?=$likes?>&nbsp;<button class="uk-icon-button uk-margin-small-right <?=$isLikedCss?>"
+                                 uk-icon="heart" uk-tooltip="<?=$isLikedTooltip?>"></button>
+    </form>
 <?endif?>
