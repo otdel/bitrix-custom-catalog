d67459c10318089a046aa65907bd48878ba35a23
Ilya Fertikov <rodeljob@gmail.com>
2020-02-17 15:47:05 +0400

    mlk-178 - компонент обработки запросов по лайкам добавлен в события инициализации приложения

--- local/include/init_app.php
+++ local/include/init_app.php
@@ -3,13 +3,17 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 /** @var $em Bitrix\Main\EventManager */
 
-$em->addEventHandler("main", "OnPageStart", function () {
-    global $APPLICATION;
+global $APPLICATION;
+
+$em->addEventHandler("main", "OnPageStart", function () use ($APPLICATION)  {
     $APPLICATION->IncludeComponent("oip:guest.user.processor.init",
         "", []);
 });
 
-$em->addEventHandler("main", "OnProlog", function () {
-    global $APPLICATION;
+$em->addEventHandler("main", "OnProlog", function () use ($APPLICATION) {
     $APPLICATION->IncludeComponent("oip:social.store.cart.processor", "", []);
+});
+
+$em->addEventHandler("main", "OnProlog", function() use ($APPLICATION) {
+    $APPLICATION->IncludeComponent("oip:relevant.products.likes.processor", "", []);
 });
