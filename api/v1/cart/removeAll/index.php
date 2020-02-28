7680e977cc1cbc7ba5362b85cb59155f649603a1
Ilya Fertikov <rodeljob@gmail.com>
2020-02-28 14:59:07 +0400

    mlk-189 - обработчик запроса на очистку корзины

---
+++ api/v1/cart/removeAll/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . "/../../../common/header.php";
+
+use Oip\ApiService\Response\Response;
+use Oip\ApiService\Response\Status;
+
+$userId = (int)$_REQUEST["cartUserId"];
+
+require  __DIR__ . "/../init.php";
+
+$cart->removeAll();
+
+$response = new Response(Status::createSuccess()->getValue());
+
+echo $response->toJSON();
+exit();
