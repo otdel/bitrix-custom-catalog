4ff6722c847627ddef4c6307b35ca9a00a197077
Ilya Fertikov <rodeljob@gmail.com>
2020-02-28 14:44:02 +0400

    mlk-189 -обработчик запроса на удаление товара

---
+++ api/v1/cart/remove/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/../../../common/header.php";
+
+use Oip\ApiService\Response\Response;
+use Oip\ApiService\Response\Status;
+
+$userId = (int)$_REQUEST["cartUserId"];
+$productId = (int)$_REQUEST["productId"];
+
+require  __DIR__ . "/../init.php";
+
+$cart->removeProduct($productId);
+
+$response = new Response(Status::createSuccess()->getValue());
+
+echo $response->toJSON();
+exit();
