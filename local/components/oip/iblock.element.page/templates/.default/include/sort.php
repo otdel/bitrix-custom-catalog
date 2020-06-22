8d49a494db03f7ba78f16a4b123ca1d1747ea987
Ilya Fertikov <rodeljob@gmail.com>
2020-06-22 12:27:39 +0400

    mlk-309 - сортировка в каталоге - добавлен вызов компонента сортировки внутри компоента-страницы списка, на странице вывода каталога включена опция вывода сортировки

--- local/components/oip/iblock.element.page/templates/.default/include/sort.php
+++ local/components/oip/iblock.element.page/templates/.default/include/sort.php
@@ -1,21 +1,7 @@
 <?php
-?>
 
-<div class="uk-inline">
-    <button class="uk-button uk-button-default uk-button-small uk-text-lowercase" type="button">
-        <i class="uk-margin-small-right" uk-icon="settings"></i>
-        текущее значение
-    </button>
+/** @var int $filterId */
 
-    <div uk-dropdown="mode: click">
-        <ul class="uk-nav uk-dropdown-nav">
-            <li class="uk-active"><a href="#">Активный пункт</a></li>
-            <li><a href="#">По рекомендациям</a></li>
-            <li><a href="#">По популярности</a></li>
-            <li><a href="#">Новые первыми</a></li>
-            <li><a href="#">Сначала дешевле</a></li>
-            <li><a href="#">Сначала дороже</a></li>
-            <li><a href="#">По скидкам</a></li>
-        </ul>
-    </div>
-</div>
+$APPLICATION->IncludeComponent("oip:sorter.form","", [
+    "FILTER_ID" => $filterId
+]);
