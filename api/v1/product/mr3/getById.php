e8d5f64d89015a032dabfb824ead0b663ae5970f
Ilya Fertikov <rodeljob@gmail.com>
2020-07-20 15:20:35 +0400

    mlk-328 - в обработчик апи по получению mr3 характеристик товара добавен цвет из свойства товара

--- api/v1/product/mr3/getById.php
+++ api/v1/product/mr3/getById.php
@@ -7,12 +7,28 @@ require __DIR__ . "/../../../common/header.php";
 require __DIR__ . "/init.php";
 
 $ids = [];
-$dbRes = CIBlockElement::GetList([],["IBLOCK_ID" => $iblockId, "PROPERTY_WARE_ID" => $wareId]);
+$properties = [];
+
+
+// выборка массива id товаров по wareId
+// и массива свойств элемента для передачи в json на фронт
+$dbRes = CIBlockElement::GetList([],["IBLOCK_ID" => $iblockId, "PROPERTY_WARE_ID" => $wareId], false, false,
+    ["IBLOCK_ID","ID","PROPERTY_COLOR", "PROPERTY_COLOR"]);
 
 while($el = $dbRes->Fetch()) {
     $ids[] = (int)$el["ID"];
+
+    $color = [
+        "featureName" => "Цвет",
+        "featureCode" => "color",
+        "featureValue" => $el["PROPERTY_COLOR_VALUE"],
+    ];
+    $properties[(int)$el["ID"]]["color"] = $color;
+
 }
 
+// выборка названий мр3 характеристик товаров
+// и значений этих характеристик
 $features = [];
 foreach ($dw->getProductFeatures() as $feature) {
     $features[$feature->getCode()] = $feature->getName();
@@ -37,6 +53,13 @@ foreach ($dw->getProductFeatureValues($ids) as $productId => $featureValues) {
         ];
         $values["features"][$featureValue->getFeatureCode()] = $features[$featureValue->getFeatureCode()];
     }
+}
+
+// добавление в итоговый массив мр3 характеристик массива свойств элементов
+$values["features"]["color"] = "Цвет";
+
+foreach ($values["values"] as $productId => $vs) {
+    $values["values"][$productId]["values"] = array_merge($vs["values"], $properties[$productId]);
 }
 
 $response = new Response(Status::createSuccess()->getValue(),json_encode($values));
