4120ed9f1a561ac0e31726bef83d57adb2cc68c9
Ilya Fertikov <rodeljob@gmail.com>
2020-08-17 14:13:57 +0400

    апгрейд конвертера дат: добавлен метод по преобразованию битриксовой даты в DateTimeImmutable

--- local/classes/Util/Bitrix/DateTimeConverter.php
+++ local/classes/Util/Bitrix/DateTimeConverter.php
@@ -16,4 +16,13 @@ class DateTimeConverter
     public function convertBitrixToNative(DateTime $dateTime): \DateTime {
         return (new \DateTime())->setTimestamp($dateTime->getTimestamp());
     }
+
+    /**
+     * @param DateTime $dateTime
+     * @return \DateTimeImmutable
+     * @throws Exception
+     */
+    public function convertBitrixToImmutable(DateTime $dateTime): \DateTimeImmutable {
+        return (new \DateTimeImmutable())->setTimestamp($dateTime->getTimestamp());
+    }
 }
